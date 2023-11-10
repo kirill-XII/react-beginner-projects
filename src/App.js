120f64b32ed96a130d0be03a16b0f7166d2b9c4f
kirill <116079986+kirill-XII@users.noreply.github.com>
2023-11-10 05:04:17 +0300

    start project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import './index.scss';
+import './index.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
-      </div>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<div>
+				<h2>Счетчик:</h2>
+				<h1>0</h1>
+				<button className='minus'>- Минус</button>
+				<button className='plus'>Плюс +</button>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
